d4d15bfa4edb8834c593527c556934a37909375a
HamdiNur <hamdoonur26@gmail.com>
2026-06-27 07:15:19 +0300

    feat: add color and pinned fields to Note model

--- src/models/Note.js
+++ src/models/Note.js
@@ -1,15 +1,21 @@
 const mongoose = require('mongoose')
-
 const noteSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true,
     trim: true
   },
-  //
   content: {
     type: String,
     required: true
+  },
+  color: {
+    type: String,
+    default: '#ffffff'
+  },
+  pinned: {
+    type: Boolean,
+    default: false
   },
   user: {
     type: mongoose.Schema.Types.ObjectId,
